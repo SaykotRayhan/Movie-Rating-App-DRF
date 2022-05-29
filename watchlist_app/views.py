e20ec756ad83ebe67509eec7c72c5bfc8cc7ef17
Rayhan <saykotrayhan199@gmail.com>
2022-05-29 22:55:54 +0600

    Started movie rating app

---
+++ watchlist_app/views.py
@@ -0,0 +1,11 @@
+# from django.shortcuts import render
+# from watchlist_app.models import Movie
+#
+#
+# # Create your views here.
+# def movie_list(request):
+#     movies = Movie.objects.all()
+#
+#
+# def movie_details(request, pk):
+#     movie = Movie.objects.get(pk=pk)
